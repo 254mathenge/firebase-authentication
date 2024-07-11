023803ee86f489a91971045a8b818bbd5b45f1c1
254mathenge <mathengefaith49@gmail.com>
2024-07-11 18:33:39 +0300

    commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,39 +1,40 @@
 /* eslint-disable no-unused-vars */
 
-import React from "react"
-import { auth, provider,providerf} from "../../config.js"
-import { signInWithPopup } from "firebase/auth"
-import {useState,useEffect} from "react"
-import "./Login.css"
+import React from "react";
+import { auth, provider, providerf } from "../../config.js";
+import { signInWithPopup } from "firebase/auth";
+import { useState, useEffect } from "react";
+import "./Login.css";
 function Login() {
-    const [value,setValue]=useState("")
-    const handleClick = () => {
-        signInWithPopup(auth,provider)((data) => {
-            setValue(data.user.email)
-            localStorage.setItem("email",data.user.email)
-        })
-    }
-    const handleFacebook = () => {
-        signInWithPopup(auth,providerf)((data) => {
-            setValue(data.user.email)
-            localStorage.setItem("email",data.user.email)
-        })
-    }
+  const [value, setValue] = useState([]);
+  const handleGoogle = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      console.log(value);
+      setValue(data.user);
+      localStorage.setItem("email", data.user);
+    });
+  };
 
-    useEffect(() => {
-        setValue(localStorage.getItem("email"))
-    })
-    console.log(value )
-    console.log(value)
-    return (
-        <div className="Login-section">
-            <h1 className="login-title">Login Form</h1>
-           
-            <button onClick={handleClick}>SignIn with Google</button>
-            <button onClick={handleFacebook}>SignIn with Facebook</button>
-            
-        </div>
-    )
+  const handleFacebook = () => {
+    signInWithPopup(auth, providerf).then((data) => {
+      console.log(value);
+      setValue(data.user);
+      localStorage.setItem("email", data.user);
+    });
+  };
+
+  useEffect(() => {
+    setValue(localStorage.getItem("email"));
+  }, [setValue]);
+
+  return (
+    <div className="Login-section">
+      <h1 className="login-title">Login Form</h1>
+
+      <button onClick={handleGoogle}>SignIn with Google</button>
+      <button onClick={handleFacebook}>SignIn with Facebook</button>
+    </div>
+  );
 }
 
 export default Login;
